a06651e1e0029a393a66b311f9f94fd34812fa52
USTGlob <87757764+USTGlob@users.noreply.github.com>
2021-07-21 15:44:56 +0530

    Add files via upload

---
+++ Java/AbstractClassExample.java
@@ -0,0 +1,19 @@
+package com.ust;
+interface a {
+	
+}
+
+interface b {
+	
+}
+class c implements a,b {
+	
+}
+public class AbstractClassExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
